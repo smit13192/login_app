f0d98b24dc31ad264a133f2175ee573dfcb3f68c
smit monpara <106581296+smit13192@users.noreply.github.com>
2023-05-25 22:49:50 +0530

    firebase connect

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login_app/app/router/app_router.dart';
 import 'package:login_app/app/router/router_name.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
       onGenerateRoute: AppRouter.onGenerateRoute,
       initialRoute: RouteName.splashScreen,
     );
--- lib/splash/views/screen/splash_screen.dart
+++ lib/splash/views/screen/splash_screen.dart
@@ -10,6 +10,10 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Screen"),
+      ),
+    );
   }
 }
